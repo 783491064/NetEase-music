42bf29627063a3f15c4937f0de041b18853c3411
bijingcun <13521319520@163.com>
2017-10-28 21:17:25 +0800

    添加代码

--- app/src/main/java/com/example/administrator/neteasemusic/MainActivity.java
+++ app/src/main/java/com/example/administrator/neteasemusic/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.neteasemusic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //TODO
+        Toast.makeText(this,"nihao",Toast.LENGTH_LONG).show();
     }
 }
